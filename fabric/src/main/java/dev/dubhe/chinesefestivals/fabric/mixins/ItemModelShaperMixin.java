7df0f117ffacf5c751db2e161a859ab258b15e38
Gu_ZT <administration@ztxy666.cn>
2023-10-18 01:18:49 +0800

    将ItemModelShaperMixin放置在fabric端，对forge端另作修改

---
+++ fabric/src/main/java/dev/dubhe/chinesefestivals/fabric/mixins/ItemModelShaperMixin.java
@@ -0,0 +1,43 @@
+package dev.dubhe.chinesefestivals.fabric.mixins;
+
+import dev.dubhe.chinesefestivals.ChineseFestivals;
+import dev.dubhe.chinesefestivals.festivals.Festivals;
+import dev.dubhe.chinesefestivals.festivals.IFestival;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.client.renderer.ItemModelShaper;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.item.Item;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Supplier;
+
+@Mixin(ItemModelShaper.class)
+public abstract class ItemModelShaperMixin {
+    @Shadow
+    @Final
+    private Int2ObjectMap<BakedModel> shapesCache;
+
+    @Shadow
+    private static int getIndex(Item item) {
+        return 0;
+    }
+
+    @Inject(method = "getItemModel(Lnet/minecraft/world/item/Item;)Lnet/minecraft/client/resources/model/BakedModel;", at = @At("HEAD"), cancellable = true)
+    private void getItemModel(Item item, CallbackInfoReturnable<BakedModel> cir) {
+        ChineseFestivals.LOGGER.info("test");
+        for (IFestival festival : Festivals.FESTIVALS) {
+            if (festival.isNow()) {
+                Supplier<Item> item1 = festival.getItemReplace().getOrDefault(item, null);
+                if (item1 != null) {
+                    cir.setReturnValue(this.shapesCache.get(getIndex(item1.get())));
+                    return;
+                }
+            }
+        }
+    }
+}
--- forge/src/main/java/dev/dubhe/chinesefestivals/forge/mixins/ForgeItemModelShaperMixin.java
+++ forge/src/main/java/dev/dubhe/chinesefestivals/forge/mixins/ForgeItemModelShaperMixin.java
@@ -1,12 +1,15 @@
-package dev.dubhe.chinesefestivals.mixins;
+package dev.dubhe.chinesefestivals.forge.mixins;
 
 import dev.dubhe.chinesefestivals.ChineseFestivals;
 import dev.dubhe.chinesefestivals.festivals.Festivals;
 import dev.dubhe.chinesefestivals.festivals.IFestival;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.client.renderer.ItemModelShaper;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.core.Holder;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.ForgeItemModelShaper;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,27 +17,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Map;
 import java.util.function.Supplier;
 
-@Mixin(ItemModelShaper.class)
-public abstract class ItemModelShaperMixin {
-    @Shadow
+@Mixin(ForgeItemModelShaper.class)
+public abstract class ForgeItemModelShaperMixin extends ItemModelShaper {
+    @Shadow(remap = false)
     @Final
-    private Int2ObjectMap<BakedModel> shapesCache;
+    private Map<Holder.Reference<Item>, BakedModel> models;
 
-    @Shadow
-    private static int getIndex(Item item) {
-        return 0;
+    public ForgeItemModelShaperMixin(ModelManager arg) {
+        super(arg);
     }
 
-    @Inject(method = "getItemModel(Lnet/minecraft/world/item/Item;)Lnet/minecraft/client/resources/model/BakedModel;", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "getItemModel", remap = false, at = @At("HEAD"), cancellable = true)
     private void getItemModel(Item item, CallbackInfoReturnable<BakedModel> cir) {
         ChineseFestivals.LOGGER.info("test");
         for (IFestival festival : Festivals.FESTIVALS) {
             if (festival.isNow()) {
                 Supplier<Item> item1 = festival.getItemReplace().getOrDefault(item, null);
                 if (item1 != null) {
-                    cir.setReturnValue(this.shapesCache.get(getIndex(item1.get())));
+                    cir.setReturnValue(this.models.get(ForgeRegistries.ITEMS.getDelegateOrThrow(item1.get())));
                     return;
                 }
             }
